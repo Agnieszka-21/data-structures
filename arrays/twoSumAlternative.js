4e0f1d3158ae26f40f81095ef17bfc59321fd768
Agnieszka-21 <agnieszkabialekmusic@gmail.com>
2025-05-18 15:24:46 +0100

    Provide an alternative solution to the twoSum problem (hash table)

---
+++ arrays/twoSumAlternative.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let numMap = new Map(); // A mapping to store numbers and their indices in a hash table
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i]; // Find the required number to reach the target
+        if (numMap.has(complement)) {
+            return [numMap.get(complement), i]; // Return indices of the complement and current number
+        }
+        numMap.set(nums[i], i); // Store the number with its index
+    }
+    return []; // This line is never reached due to the problem guarantee
+};
+
+console.log(twoSum([3, 2, 4], 6));
+
+// Time and space complexity of O(n)
