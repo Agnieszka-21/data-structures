4fa498d5edc577bd16228ddfe83347ab08b8ae77
Agnieszka-21 <agnieszkabialekmusic@gmail.com>
2025-05-22 08:46:44 +0100

    Rotate an array to the right by a specified number of steps

---
+++ arrays/rotateArray.js
@@ -0,0 +1,16 @@
+/**
+ * Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    for (let i = 0; i < k; i++) {
+        const intNum = nums.pop();
+        nums.unshift(intNum);
+    }
+
+    return nums;
+};
+
+console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));
